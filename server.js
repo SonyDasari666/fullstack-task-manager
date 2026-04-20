2a6f2e20cbc55d1dc14cc378b276fc1e0aa51003
SonyDasari666 <sonydasari230@gmail.com>
2026-04-20 11:15:11 +0530

    fix cors

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/tasks", require("./routes/tasks"));
